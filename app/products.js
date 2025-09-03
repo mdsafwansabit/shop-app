d6755eef7a5f130dd0c5d1502201327c5cec79bb
Safwan Sabit <mdsafwansabitofficial@gmail.com>
2025-09-03 08:00:54 +0000

    update

--- app/products.js
+++ app/products.js
@@ -3,6 +3,7 @@ const products = [
   { id: 2, name: "Toy Car", price: 850, img: "https://images.othoba.com/images/thumbs/0494457_car-toy-for-kids.jpeg" },
   { id: 3, name: "School Bag", price: 2000, img: "https://images.othoba.com/images/thumbs/0674306_children-premium-school-bag.webp" },
   { id: 4, name: "Shoes", price: 1500, img: "https://bd-4.edkncdn.net/img/800x800/stores/6319bca140641aa1ae1ee667/product-square/1698609976232-enuOuSe11zM8P10QbycXW/SOLE%20SURGE%20WARRIOR%20SHOES%20(3).webp" },
+  { id: 5, name: "A9 Pro ANC Touch Screen Display Earbuds", price: 1200, img: "https://liliputshopbd.com/wp-content/uploads/A9-Pro-ANC-White-800x800-1.avif" },
 ];
 
 export default products;
